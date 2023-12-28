e3258fcfa4800b320f320227315aa3aad6d23813
0xrin1 <0xrin1@protonmail.com>
2023-12-28 16:31:00 -0500

    basic tokenomics sections

--- src/Tokenomics.tsx
+++ src/Tokenomics.tsx
@@ -12,9 +12,12 @@ const styles = stylex.create({
     marginTop: "4rem",
     backgroundColor: colors.teal4,
     clipPath: "polygon(0 1%,100% 0,100% 100%,0 95%);",
-
+    paddingTop: "4rem",
+    paddingBottom: "4rem",
   },
-
+  title: {
+    marginBottom: "2rem",
+  },
 });
 
 type Props = {
@@ -23,13 +26,13 @@ type Props = {
 
 export default function Tokenomics({ style }: Props) {
   return <div {...stylex.props(styles.default, style)}>
-    <u>Tokenomics</u>
+    <u {...stylex.props(styles.title, style)}>Tokenomics</u>
     <ul>
-      <li>Lorem</li>
-      <li>ipsum</li>
-      <li>dolor</li>
-      <li>sit</li>
-      <li>amet</li>
+      <li>100mm supply</li>
+      <li>Initial LP Burned</li>
+      <li>Initial Supply Burn by dev ~11mm MINGUS</li>
+      <li>No team distribution</li>
+      <li>No distributions at all.</li>
     </ul>
   </div>;
 }
